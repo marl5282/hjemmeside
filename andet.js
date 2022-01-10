a4743e362cb13f164d415d401071ecdfeceffafc
Marlene Merkel <marl5282@stud.kea.dk>
2022-01-10 19:03:22 +0100

    indexsite

--- andet.js
+++ andet.js
@@ -1,35 +1,48 @@
 // ------------------------BURGER MENU-----------------------------
 
-// Lav en variabel, der refererer til ".toggle-btn"
-const btn = document.querySelector(".toggle-btn");
-// Lav en variabel, der refererer til "nav"
-const nav = document.querySelector("nav");
+// // Lav en variabel, der refererer til ".toggle-btn"
+// const btn = document.querySelector(".toggle-btn");
+// // Lav en variabel, der refererer til "nav"
+// const nav = document.querySelector("nav");
 
-// Lav en function, der hedder toggleMenu()
+// // Lav en function, der hedder toggleMenu()
+// function toggleMenu() {
+//   // 1. Toggle en klasse på nav vha. classList.toggle
+//   // 2. Toggle en klasse, der hedder "shown"
+//   nav.classList.toggle("shown");
+
+//   // 1. Lav en variabel, der hedder menuShown
+//   // 2. Den skal være lig med, om nav-variablen indeholder klassen "shown" vha. classList.contains("")
+//   const menuShown = nav.classList.contains("shown");
+//   // 1. Lav en if/else sætning => if (...) {...} else {...}
+
+//   // 2. Spørg om menu i if-sætningen => if (menu)
+//   if (menuShown) {
+//     btn.textContent = "Luk";
+//     // hvis nav har klassen "shown", sæt da btn.textContent til "Luk"
+//   } else {
+//     btn.textContent = "Menu";
+//     // hvis IKKE nav har klassen "shown", sæt da btn.textContent til "Menu"
+//   }
+// }
+
+const btn = document.querySelector("button");
+const ul = document.querySelector("nav");
+
 function toggleMenu() {
-  // 1. Toggle en klasse på nav vha. classList.toggle
-  // 2. Toggle en klasse, der hedder "shown"
-  nav.classList.toggle("shown");
+  ul.classList.toggle("shown");
 
-  // 1. Lav en variabel, der hedder menuShown
-  // 2. Den skal være lig med, om nav-variablen indeholder klassen "shown" vha. classList.contains("")
-  const menuShown = nav.classList.contains("shown");
-  // 1. Lav en if/else sætning => if (...) {...} else {...}
+  const menu = ul.classList.contains("shown");
 
-  // 2. Spørg om menu i if-sætningen => if (menu)
-  if (menuShown) {
-    btn.textContent = "Luk";
-    // hvis nav har klassen "shown", sæt da btn.textContent til "Luk"
+  if (menu) {
+    // hvis ul har klassen "shown"
+    //btn.textContent = "Luk";
+    btn.classList.add("open");
   } else {
-    btn.textContent = "Menu";
-    // hvis IKKE nav har klassen "shown", sæt da btn.textContent til "Menu"
+    // hvis IKKE ul har klassen "shown"
+    //btn.textContent = "Menu";
+    btn.classList.remove("open");
   }
 }
 
-/* Tilføj et klik-event til btn, der sætter toggleMenu-funktionen i gang */
 btn.addEventListener("click", toggleMenu);
-
-//Startskærm- click for at komme videre til anden side.
-//Klik på start_knap
-console.log("startGame");
-document.querySelector("#start_knap").addEventListener("click", startGame);
